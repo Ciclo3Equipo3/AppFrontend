8e5a02418e59a19b2633fada3e416f1fad433219
BrayDC04 <92704386+BrayDC04@users.noreply.github.com>
2021-10-17 23:15:34 -0500

    Add files via upload

--- boringShopFront/src/main/java/co/edu/unbosque/front/Controlador.java
+++ boringShopFront/src/main/java/co/edu/unbosque/front/Controlador.java
@@ -1,19 +1,26 @@
 package co.edu.unbosque.front;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-import co.edu.unbosque.front.Clientes;
-import co.edu.unbosque.front.ClientesTestJSON;
-import co.edu.unbosque.front.Proveedores;
-import co.edu.unbosque.front.ProveedoresTestJSON;
+import com.csvreader.CsvReader;
+
+
+
+
+
+
 
 /**
  * Servlet implementation class Controlador
@@ -145,7 +152,7 @@ public class Controlador extends HttpServlet {
 	    			 respuesta = ClientesTestJSON.postJSON(cliente);
 	    			 PrintWriter write = response.getWriter();
 	    			 if (respuesta==200) {
-	    				 request.getRequestDispatcher("Controller?menu=Clientes&accion=Listar").forward(request,
+	    				 request.getRequestDispatcher("Controlador?menu=Clientes&accion=Listar").forward(request,
 	                                                                               response);
 	    			 } else {
 	    				 write.println("Error: " +  respuesta);
@@ -169,7 +176,7 @@ public class Controlador extends HttpServlet {
 		        	  PrintWriter write = response.getWriter();
 								
 		        	  if (respuesta==200) {
-		        		  request.getRequestDispatcher("Controller?menu=Clientes&accion=Listar").forward(request, response);
+		        		  request.getRequestDispatcher("Controlador?menu=Clientes&accion=Listar").forward(request, response);
 		        	  } else {
 		        		  write.println("Error: " +  respuesta);
 		        	  }
@@ -185,7 +192,7 @@ public class Controlador extends HttpServlet {
 					 for (Clientes clientes:lista1){
 						if (clientes.getCedula_cliente()==id) {
 							request.setAttribute("clienteSeleccionado", clientes);
-							request.getRequestDispatcher("Controller?menu=Clientes&accion=Listar").forward(request, response);	
+							request.getRequestDispatcher("Controlador?menu=Clientes&accion=Listar").forward(request, response);	
 						}
 					}
 				 } catch (Exception e) {
@@ -200,7 +207,7 @@ public class Controlador extends HttpServlet {
 				   respuesta = ClientesTestJSON.deleteJSON(id);
 				   PrintWriter write = response.getWriter();
 				   if (respuesta==200) {
-					   request.getRequestDispatcher("Controller?menu=Clientes&accion=Listar").forward(request, response);
+					   request.getRequestDispatcher("Controlador?menu=Clientes&accion=Listar").forward(request, response);
 				   } else {
 					   write.println("Error: " +  respuesta);
 				   }
@@ -232,7 +239,7 @@ public class Controlador extends HttpServlet {
 	    			 respuesta = ProveedoresTestJSON.postJSON(proveedor);
 	    			 PrintWriter write = response.getWriter();
 	    			 if (respuesta==200) {
-	    				 request.getRequestDispatcher("Controller?menu=Proveedores&accion=Listar").forward(request,
+	    				 request.getRequestDispatcher("Controlador?menu=Proveedores&accion=Listar").forward(request,
 	                                                                               response);
 	    			 } else {
 	    				 write.println("Error: " +  respuesta);
@@ -256,7 +263,7 @@ public class Controlador extends HttpServlet {
 		        	  PrintWriter write = response.getWriter();
 								
 		        	  if (respuesta==200) {
-		        		  request.getRequestDispatcher("Controller?menu=Proveedores&accion=Listar").forward(request, response);
+		        		  request.getRequestDispatcher("Controlador?menu=Proveedores&accion=Listar").forward(request, response);
 		        	  } else {
 		        		  write.println("Error: " +  respuesta);
 		        	  }
@@ -272,7 +279,7 @@ public class Controlador extends HttpServlet {
 					 for (Proveedores proveedores:lista1){
 						if (proveedores.getNitproveedor()==id) {
 							request.setAttribute("proveedorSeleccionado", proveedores);
-							request.getRequestDispatcher("Controller?menu=Proveedores&accion=Listar").forward(request, response);	
+							request.getRequestDispatcher("Controlador?menu=Proveedores&accion=Listar").forward(request, response);	
 						}
 					}
 				 } catch (Exception e) {
@@ -287,7 +294,7 @@ public class Controlador extends HttpServlet {
 				   respuesta = ProveedoresTestJSON.deleteJSON(id);
 				   PrintWriter write = response.getWriter();
 				   if (respuesta==200) {
-					   request.getRequestDispatcher("Controller?menu=Proveedores&accion=Listar").forward(request, response);
+					   request.getRequestDispatcher("Controlador?menu=Proveedores&accion=Listar").forward(request, response);
 				   } else {
 					   write.println("Error: " +  respuesta);
 				   }
@@ -304,14 +311,149 @@ public class Controlador extends HttpServlet {
 	     case "Ventas":	
 	    	 request.getRequestDispatcher("/Ventas.jsp").forward(request, response);
 	    	 break;
-		}
-
+	     case "Reportes":
+             try {
+	    	 int opcion=0;
+	    	 if (accion.equals("ReporteUsuarios")) {
+					opcion=1;
+					try {
+						ArrayList<Usuarios> lista = UsuarioJSON.getJSON();
+						request.setAttribute("lista", lista); //envio el arraylist 
+						request.setAttribute("opcion", opcion); //variable creada
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}else if (accion.equals("ReporteClientes")) {
+					opcion=2;
+					try {
+						ArrayList<Clientes> lista = ClientesTestJSON.getJSON();
+						request.setAttribute("lista", lista); //envio el arraylist 
+						request.setAttribute("opcion", opcion); //variable creada
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}else if (accion.equals("ReporteVentas")) {
+					opcion=3;
+					try {
+						//ArrayList<Ventas> lista = VentasTestJSON.getJSON();
+						//request.setAttribute("listaVentas", lista); //envio el arraylist 
+						//request.setAttribute("opcion", opcion); //variable crada
+					} catch (Exception e) {
+						e.printStackTrace();
+				}
+				}
+	    	 }catch (Exception e) {
+					e.printStackTrace();
+				}
+	    	 request.getRequestDispatcher("/Reportes.jsp").forward(request, response);
+				break;
+	    	 }
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+		PrintWriter out = response.getWriter();
+		System.out.println("Entro Sevlet");
+		String nomb = request.getParameter("nombre");
+		Part arch = request.getPart("archivo");
+		String process = request.getParameter("Procesar");
+		InputStream is = arch.getInputStream();
+		Charset charset = Charset.forName("UTF-8");
+		
+		if (process != null) {
+			if(is.available() ==0) {
+				is.close();
+				System.out.println("Entro error sin archivo");
+				request.setAttribute("error", 1);//error no se ha seleccionado el archivo
+				request.getRequestDispatcher("/Productos.jsp").forward(request, response);
+			}else {
+				if(ProductosTestJSON.validarCSV(nomb)) {
+					try {
+						String registros = "";
+						boolean reg_no_cargados = false;
+						CsvReader leerproducto = new CsvReader(is, charset);
+						leerproducto.readHeaders();
+						List<Productos> productos = new ArrayList<Productos>(); // Lista donde se guardara los datos
+						while(leerproducto.readRecord()) {
+							String codigo = leerproducto.get(0);
+							String iva = leerproducto.get(1);
+							String nit = leerproducto.get(2);
+							String nombre = leerproducto.get(3);
+							String compra = leerproducto.get(4);
+							String venta = leerproducto.get(5);
+							
+							//añade lo leido a una lista tipo productios
+						productos.add(new Productos(Long.parseLong(codigo), Double.parseDouble(iva), Long.parseLong(nit), nombre, Double.parseDouble(compra), Double.parseDouble(venta)));
+						}
+						is.close();
+						System.out.println("Entro en boton");
+						ArrayList<Productos> listafinal = new ArrayList<Productos>();
+						try {
+							
+							ArrayList<Proveedores> lista = ProveedoresTestJSON.getJSON();
+							for (Proveedores suplier:lista) {
+								for(Productos item:productos) {
+									if(suplier.getNitproveedor() == item.getNitproveedor()) {
+										System.out.println("Entro for if");
+										listafinal.add(item);
+									}else {
+										registros = String.valueOf(item.getNitproveedor())+",";
+										reg_no_cargados =  true;
+									}
+								}
+							}
+						} catch (Exception e) {
+							System.out.println("Entro catch");
+							e.printStackTrace();
+							// TODO: handle exception
+						}
+						String registros2 = "";
+						int ban= 3;
+						int validacion = 0;
+						for(Productos item:listafinal) {
+							ArrayList<Productos> listaproductos = ProductosTestJSON.getJSON();
+							for(Productos item2:listaproductos) {
+								if(item.getCodigo_producto()!=item2.getCodigo_producto()) {
+									validacion = ProductosTestJSON.postJSON(item);
+									if(validacion==200) {
+										ban = (ban * 0) + 2;
+									}
+								}else {
+									registros2 = String.valueOf(item.getCodigo_producto())+",";
+								}
+							}		
+						}
+						if(validacion == 200) {
+							System.out.println("carga existosa");
+							request.setAttribute("error", 2); //carga exitosa de csv
+							request.getRequestDispatcher("/Productos.jsp").forward(request, response);
+						}else {
+							System.out.println("los registros no se cargaron");
+							request.setAttribute("error", 4); //Registros no se cargaron
+							request.getRequestDispatcher("/Productos.jsp").forward(request, response);
+						}
+					}catch(Exception e){
+						System.out.println("Entro datos invalidos");
+						request.setAttribute("error", 0);//error datos leidos invalidos
+						request.getRequestDispatcher("/Productos.jsp").forward(request, response);
+					}finally {
+						is.close();
+					}
+				}else {
+					is.close();
+					System.out.println("Entro error formato");
+					request.setAttribute("error", 3);//error datos leidos invalidos
+					request.getRequestDispatcher("Productos.jsp").forward(request, response);
+					//request.getRequestDispatcher("/ValidacionProductos?validar=3&error=3").forward(request, response);
+				}
+			}		
+		
+		}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
